68dd8fa65858f4dd656234f2ef70fbb90bf61404
Kavya shri Borra <kavya.chow@gmail.com>
2025-04-12 00:14:54 -0500

    Simple HTTP Methods using Pydantic Model

--- carshare.py
+++ carshare.py
@@ -1,28 +1,19 @@
 import uvicorn
 from fastapi import FastAPI, Query, HTTPException
 from typing import Optional, List
+from schemas import load_db, save_db, CarInput, CarOutput
 
 app = FastAPI()
-db = [
-    {"id": 1, "size": "s", "fuel": "gasoline", "doors": 3, "transmission": "auto"},
-    {"id": 2, "size": "s", "fuel": "electric", "doors": 3, "transmission": "auto"},
-    {"id": 3, "size": "s", "fuel": "gasoline", "doors": 5, "transmission": "manual"},
-    {"id": 4, "size": "m", "fuel": "electric", "doors": 3, "transmission": "auto"},
-    {"id": 5, "size": "m", "fuel": "hybrid", "doors": 5, "transmission": "auto"},
-    {"id": 6, "size": "m", "fuel": "gasoline", "doors": 5, "transmission": "manual"},
-    {"id": 7, "size": "l", "fuel": "diesel", "doors": 5, "transmission": "manual"},
-    {"id": 8, "size": "l", "fuel": "electric", "doors": 5, "transmission": "auto"},
-    {"id": 9, "size": "l", "fuel": "hybrid", "doors": 5, "transmission": "auto"}
-]
+db = load_db()
 
 
 # optional parameter to get call
 @app.get("/api/cars")
 async def get_cars(size: Optional[str] = None, doors: Optional[int] = None):
     if size:
-        return [car for car in db if car['size'] == size]
+        return [car for car in db if car.size == size]
     if doors:
-        return [car for car in db if car['doors'] >= doors]
+        return [car for car in db if car.doors >= doors]
 
     return db
 
@@ -31,10 +22,46 @@ async def get_cars(size: Optional[str] = None, doors: Optional[int] = None):
 
 @app.get("/api/cars/{id}")
 def get_car_by_id(id: int):
-    car_result = [car for car in db if car["id"] == id]
+    car_result = [car for car in db if car.id == id]
     if not car_result:
         raise HTTPException(status_code=404, detail=f"Car with ID {id} not found.")
     return car_result[0]
+
+
+@app.post("/api/cars/")
+def add_car(car: CarInput):
+    new_car = CarOutput(size=car.size, doors=car.doors,
+                        fuel=car.fuel, transmission=car.transmission,
+                        id=len(db) + 1)
+    db.append(new_car)
+    save_db(db)
+    return new_car
+
+
+@app.put("/api/cars/{id}")
+def update_car_info(id: int, new_data: CarInput):
+    car_to_update = [car for car in db if car.id == id]
+    if car_to_update:
+        res = car_to_update[0]
+        res.fuel = new_data.fuel
+        res.size = new_data.size
+        res.doors = new_data.doors
+        res.transmission = new_data.transmission
+        save_db(db)
+        return res
+    else:
+        raise HTTPException(status_code=404, details=f"No car with id={id}")
+
+
+@app.delete("/api/cars/{id}", status_code=204)
+def delete_car_record(id: int):
+    car_to_del = [car for car in db if car.id == id]
+    if car_to_del:
+        results = car_to_del[0]
+        db.remove(results)
+        save_db(db)
+    else:
+        raise HTTPException(status_code=404, details=f"No car with id={id}")
 
 
 if __name__ == "__main__":
